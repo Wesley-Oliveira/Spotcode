e1da7148ce318295cffc22d20d02f3d25ad4fc29
Wesley-Oliveira <wesley0leby@gmail.com>
2020-04-18 10:49:55 -0300

    Creating the search page

---
+++ app/javascript/components/commom/categories/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+const Categories = () => {
+    
+    return (
+        <Fragment>
+        ​
+        </Fragment>
+    );
+}
+
+export default Categories;
--- app/javascript/screens/search/index.js
+++ app/javascript/screens/search/index.js
@@ -1,9 +1,16 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import SectionWrapper from '../../components/commom/section_wrapper'
+import { Heading } from 'react-bulma-components';
+import Search from '../../components/search';
 import NavbarFooter from '../../components/commom/navbar_footer';
 
 const SearchScreen = () => {
-  return(
+  return (
     <Fragment>
+      <SectionWrapper>
+        <Heading className='has-text-centered has-text-white'>Buscar</Heading>
+        <Search/>
+      </SectionWrapper>
       <NavbarFooter/>
     </Fragment>
   );
