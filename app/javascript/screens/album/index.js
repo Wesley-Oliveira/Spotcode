f66ec6436ece671f8749860fb5e3b8c2f3ce6a26
Wesley-Oliveira <wesley0leby@gmail.com>
2020-04-18 07:25:19 -0300

    Creating the Album page

--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,10 +1,16 @@
 import React, { Fragment, useState } from 'react';
 import NavbarFooter from '../../components/commom/navbar_footer';
+import Albums from '../../components/albums';
+import SectionWrapper from '../../components/commom/section_wrapper';
+
 
 const AlbumScreen = () => {
   return(
     <Fragment>
-      <NavbarFooter/>
+      <SectionWrapper>
+        <Albums/>
+        <NavbarFooter/>
+      </SectionWrapper>
     </Fragment>
   );
 }
